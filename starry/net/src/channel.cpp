d3c10b188e5faea07486d0fd7913956a7dcf3f18
aiden <aiden.yuan.dev@gmail.com>
2024-11-19 06:19:25 -0500

    channel bug

--- starry/net/src/channel.cpp
+++ starry/net/src/channel.cpp
@@ -14,8 +14,8 @@
 using namespace starry;
 
 const int Channel::kNoneEvent = 0;
-const int Channel::kReadEvent = POLLIN | POLLPRI;
-const int Channel::kWriteEvent = POLLOUT;
+const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::kWriteEvent = EPOLLOUT;
 
 Channel::Channel(EventLoop* loop, int fd)
     : loop_(loop),
